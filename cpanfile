1a867d8fd8f4b34fe22b7efc8a02ee9f0582860e
Erik Carlsson <info@code301.com>
2014-09-07 19:23:57 +0000

    Import of CSSON/Dist-Zilla-Plugin-Test-CreateFromMojoTemplates-0.03 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-Test-CreateFromMojoTemplates
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         CSSON/Dist-Zilla-Plugin-Test-CreateFromMojoTemplates-0.03.tar.gz
    gitpan-cpan-author:       CSSON
    gitpan-cpan-maturity:     released

--- cpanfile
+++ cpanfile
@@ -4,7 +4,7 @@ requires 'Moose', '2.00';
 requires 'File::Find::Rule', '0.33';
 requires 'namespace::sweep', '0.006';
 requires 'Path::Tiny', '0.050';
-requires 'MojoX::CustomTemplateFileParser', '0.02';
+requires 'MojoX::CustomTemplateFileParser', '0.04';
 
 requires 'Dist::Zilla::File::InMemory', '5.000';
 requires 'Dist::Zilla::Role::FileGatherer', '5.000';
--- lib/Dist/Zilla/Plugin/Test/CreateFromMojoTemplates.pm
+++ lib/Dist/Zilla/Plugin/Test/CreateFromMojoTemplates.pm
@@ -2,7 +2,7 @@ package Dist::Zilla::Plugin::Test::CreateFromMojoTemplates;
 
 use strict;
 use 5.10.1;
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use Moose;
 use File::Find::Rule;
@@ -24,7 +24,6 @@ has filepattern => (
     default => sub { '^\w+-\d+\.mojo$' },
 );
 
-
 sub gather_files {
     my $self = shift;
     my $arg = shift;
@@ -36,12 +35,10 @@ sub gather_files {
     foreach my $path (@paths) {
 
         my $contents = MojoX::CustomTemplateFileParser->new(path => path($path)->absolute)->parse->flatten;
-
-        $path =~ s{\.mojo$}{.t};
-        $path =~ s{.*/([^/]*$)}{t/$1};
+        my $filename = path($path)->basename(qr{\.[^.]+});
 
         my $file = Dist::Zilla::File::InMemory->new(
-            name => $path,
+            name => "t/$filename.t",
             content => $test_template . $contents,
         );
         $self->add_file($file);
@@ -49,152 +46,6 @@ sub gather_files {
     }
 
     return;
-}
-
-sub prepare {
-    my $path = shift;
-    my @lines = split /\n/ => path($path)->slurp;
-
-    (my $filename = $path) =~ s{.*/([^/]*$)}{$1};      # remove path
-    (my $baseurl = $filename) =~ s{^([^\.]+)\..*}{$1}; # remove suffix
-    $baseurl =~ s{-}{_};
-
-    my $info = parse_source($baseurl, @lines);
-
-    my @parsed = join "\n" => @{ $info->{'head_lines'} };
-
-    my $testcount = 0;
-    foreach my $test (@{ $info->{'tests'} }) {
-        ++$testcount;
-        my $expected_var = sprintf '$expected_%s' => $testcount;
-        push @parsed => sprintf 'my %s = qq{ %s };' => $expected_var, join "\n" => @{ $test->{'lines_expected'} };
-
-        push @parsed => sprintf q{get '/%s' => '%s';} => $test->{'test_name'}, $test->{'test_name'};
-        push @parsed => sprintf q{$test->get_ok('/%s')->status_is(200)->trimmed_content_is(%s, '%s');}
-                                => $test->{'test_name'}, $expected_var, qq{Matched trimmed content in $filename, line $test->{'test_start_line'}};
-    }
-
-    push @parsed => 'done_testing();';
-    push @parsed => '__DATA__';
-
-    foreach my $test (@{ $info->{'tests'} }) {
-        push @parsed => sprintf '@@ %s.html.ep' => $test->{'test_name'};
-        push @parsed => join "\n" => @{ $test->{'lines_template'} };
-    }
-
-    return join "\n\n" => @parsed;
-
-}
-
-sub parse_source {
-    my $baseurl = shift;
-    my @lines = @_;
-
-    my $test_start = qr/==TEST(?: EXAMPLE)?(?: (\d+))?==/i;
-    my $template_separator = '--t--';
-    my $expected_separator = '--e--';
-
-    my $environment = 'head';
-
-    my $info = {
-        head_lines => [],
-        tests      => []
-    };
-    my $test = {};
-
-    my $row = 0;
-    my $testcount = 0;
-
-    LINE:
-    foreach my $line (@lines) {
-        ++$row;
-
-        if($environment eq 'head') {
-            if($line =~ $test_start) {
-                $test = reset_test();
-                $test->{'test_number'} = $1;
-                ++$testcount;
-
-                push @{ $info->{'head_lines'} } => '';
-                $test->{'test_start_line'} = $row;
-                $test->{'test_number'} = $testcount;
-                $test->{'test_name'} = sprintf '%s_%s' => $baseurl, $testcount;
-                $environment = 'beginning';
-
-                next LINE;
-            }
-            push @{ $info->{'head_lines'} } => $line;
-            next LINE;
-        }
-        if($environment eq 'beginning') {
-            if($line eq $template_separator) {
-                push @{ $test->{'lines_before'} } => '';
-                $environment = 'template';
-                next LINE;
-            }
-            push @{ $test->{'lines_before'} } => $line;
-            next LINE;
-        }
-        if($environment eq 'template') {
-            if($line eq $template_separator) {
-                # No need to push empty line to the template
-                $environment = 'between';
-                next LINE;
-            }
-            push @{ $test->{'lines_template'} } => $line;
-            next LINE;
-        }
-        if($environment eq 'between') {
-            if($line eq $expected_separator) {
-                push @{ $test->{'lines_between'} } => '';
-                $environment = 'expected';
-                next LINE;
-            }
-            push @{ $test->{'lines_expected'} } => $line;
-            next LINE;
-        }
-        if($environment eq 'expected') {
-            if($line eq $expected_separator) {
-                # No need to push empty line to the template
-                $environment = 'ending';
-                next LINE;
-            }
-            push @{ $test->{'lines_expected'} } => $line;
-            next LINE;
-        }
-        if($environment eq 'ending') {
-            if($line =~ $test_start) {
-                push @{ $test->{'lines_after'} } => '';
-                push @{ $info->{'tests'} } => $test;
-                $test = reset_test();
-                ++$testcount;
-                $test->{'test_start_line'} = $row;
-                $test->{'test_number'} = $testcount;
-                $test->{'test_name'} = sprintf '%s_%s' => $baseurl, $testcount;
-                $environment = 'beginning';
-
-                next LINE;
-            }
-            push @{ $test->{'lines_after'} } => $line;
-            next LINE;
-        }
-    }
-    push @{ $info->{'tests'} } => $test;
-
-    return $info;
-}
-
-sub reset_test {
-    return {
-        lines_before => [],
-        lines_template => [],
-        lines_after => [],
-        lines_between => [],
-        lines_expected => [],
-        test_number => undef,
-        test_start_line => undef,
-        test_name => undef,
-    };
 }
 
 __PACKAGE__->meta->make_immutable;
